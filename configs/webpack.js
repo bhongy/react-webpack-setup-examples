f59dd2ba393c167ea04ac4b6bbf91558fe03438d
Thanik Bhongbhibhat <thanik@b8ta.com>
2017-05-04 20:00:46 -0700

    Add Babel Stage-2 support

--- configs/webpack.js
+++ configs/webpack.js
@@ -24,6 +24,7 @@ module.exports = (env) => ({
           babelrc: false,
           presets: [
             ['es2015', { modules: false }],
+            'stage-2',
             'react',
           ],
         },
