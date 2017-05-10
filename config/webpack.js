1e5c55d669bd08743b1cf4c0208c9b0cb103af38
Thanik Bhongbhibhat <thanik@b8ta.com>
2017-05-10 08:12:37 -0700

    Refactor Webpack config to make it easier to read (but less dry)

--- config/webpack.js
+++ config/webpack.js
@@ -1,52 +1,13 @@
+const { noop } = require('lodash')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 const project = require('../config/project')
-const { noop } = require('lodash')
 
-module.exports = ({
-  devServer = false,
-  production = false,
-}) => {
-  const config = {
-    context: project.paths.src(),
-  }
-
-  const mainEntry = devServer
-    ? [
-      // activate HMR for React
-      'react-hot-loader/patch',
-      // bundle the client for webpack-dev-server
-      // and connect to the provided endpoint
-      'webpack-dev-server/client',
-      // bundle the client for hot reloading
-      // only- means to only hot reload for successful updates
-      'webpack/hot/only-dev-server',
-      './index.js'
-    ]
-    : './index.js'
-
-  config.entry = {
-    main: mainEntry,
-    vendor: ['react', 'react-dom'],
-  }
-
-  config.output = {
-    // use [chunkhash] in production only
-    // because it increases compilation time
-    filename: production ? '[name].[chunkhash:12].js' : '[name].js',
-    path: project.paths.dist(),
-    // necessary for HMR to know where to load the hot update chunks
-    publicPath: devServer
-      ? '/'
-      : production
-      ? 'https://cdn.google.com'
-      : '',
-  }
-
-  config.module = {
+const commonConfig = {
+  context: project.paths.src(),
+  module: {
     rules: [{
       test: /\.js$/,
       loader: 'babel-loader',
@@ -60,72 +21,120 @@ module.exports = ({
         options: { modules: true },
       }],
     }]
-  }
+  },
+}
 
-  const commonsChunk = () =>
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor', // specify the common bundle's name.
-    //   // implicit common vendor chunk
-    //   // https://webpack.js.org/guides/code-splitting-libraries/#implicit-common-vendor-chunk
-    //   minChunks(module) {
-    //     return module.context && module.context.indexOf('node_modules') !== -1
-    //   },
-    // }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'manifest',
-    // }),
+const devServerConfig = {
+  entry: {
+    main: [
+      // activate HMR for React
+      'react-hot-loader/patch',
+      // bundle the client for webpack-dev-server
+      // and connect to the provided endpoint
+      'webpack-dev-server/client',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
+      'webpack/hot/only-dev-server',
+      // TODO: extract to variable
+      './index.js'
+    ],
+    // TODO: extract to variable
+    vendor: ['react', 'react-dom'],
+  },
+
+  output: {
+    filename: '[name].js',
+    path: project.paths.dist(),
+    // necessary for HMR to know where to load the hot update chunks
+    publicPath: '/',
+  },
+
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    // enable HMR on the server
+    hot: true,
+    contentBase: project.paths.dist(),  // output.path
+    publicPath: '/',  // output.publicPath
+  },
+
+  plugins: [
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+    // otherwise HMR will log module id instead
+    new webpack.NamedModulesPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       // important: "manifest" has to follow "vendor"
       // otherwise it will be included in "vendor" and cause
       // vendor [chunkhash] to change everytime
       names: ['vendor', 'manifest'],
-    })
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Demo',
+      template: project.paths.src('index.ejs'),
+    }),
+  ]
+}
 
-  const cleanDist = () =>
+const buildConfig = {
+  entry: {
+    // main: ['./index.js'],
+    main: './index.js',
+    vendor: ['react', 'react-dom'],
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
     new CleanWebpackPlugin([
       project.paths.dist(),
     ], {
       root: project.paths.root(),
-    })
-
-  const htmlPlugin = () =>
+    }),
     new HtmlWebpackPlugin({
       title: 'Webpack Demo',
       template: project.paths.src('index.ejs'),
-    })
+    }),
+    new InlineManifestWebpackPlugin({
+      name: 'webpackManifest',
+    }),
+  ]
+}
 
-  if (devServer) {
-    config.plugins = [
-      // enable HMR globally
-      new webpack.HotModuleReplacementPlugin(),
-      // prints more readable module names in the browser console on HMR updates
-      new webpack.NamedModulesPlugin(),
-      commonsChunk(),
-      htmlPlugin(),
-    ]
-  } else {
-    config.plugins = [
-      commonsChunk(),
-      cleanDist(),
-      htmlPlugin(),
-      new InlineManifestWebpackPlugin({
-        name: 'webpackManifest',
-      }),
-    ]
+const debugBuildConfig = {
+  output: {
+    filename: '[name].js',
+    path: project.paths.dist(),
+    publicPath: '',
+  },
+}
+
+const productionBuildConfig = {
+  output: {
+    // use [chunkhash] in production only
+    // because it increases compilation time
+    filename: '[name].[chunkhash:12].js',
+    path: project.paths.dist(),
+    publicPath: 'https://cdn.google.com',
+  },
+}
+
+module.exports = (environment) => {
+  const env = Object.assign({
+    devServer: false,
+    production: false,
+  }, environment)
+
+  if (env.devServer) {
+    return Object.assign({}, commonConfig, devServerConfig)
   }
 
-  config.devtool = production
-      ? 'hidden-source-map'
-      : 'cheap-module-eval-source-map'
-
-  if (devServer) {
-    config.devServer = {
-      // enable HMR on the server
-      hot: true,
-      contentBase: config.output.path,
-      publicPath: config.output.publicPath,
-    }
+  // !devServer -> build (debug)
+  if (!env.production) {
+    return Object.assign({}, commonConfig, buildConfig, debugBuildConfig)
   }
 
-  return config
+  // !devServer -> build (production)
+  return Object.assign({}, commonConfig, buildConfig, productionBuildConfig)
 }
