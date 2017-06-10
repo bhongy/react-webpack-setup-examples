0824f1ae7b8762f864aae7b80a13f647a5ce7a1f
Thanik Bhongbhibhat <thanik@b8ta.com>
2017-06-10 10:59:26 -0700

    Create simple AsyncComponent

--- src/app.js
+++ src/app.js
@@ -1,25 +1,14 @@
 import React, { PureComponent } from 'react'
+import asyncComponent from './async-component'
 import Header from './header'
 import s from './app.css'
 
-/* eslint-disable react/jsx-no-bind */
+const AsyncFooter = asyncComponent(() =>
+  import('./footer').then(module => module.default),
+)
+
+/* eslint-disable */
 class App extends PureComponent {
-  state = {
-    footer: null,
-  }
-
-  componentDidMount() {
-    // need babel-presets-stage-3 or babel-plugin-syntax-dynamic-import
-    // to prevent parse error for dynamic import
-    // https://webpack.js.org/guides/code-splitting-async/#usage-with-babel
-    import('./footer')
-      .then(module => module.default)
-      .then((Footer) => {
-        this.setState({ footer: <Footer /> })
-      })
-      .catch(() => null)
-  }
-
   render() {
     return (
       <main className={s.page}>
@@ -28,7 +17,7 @@ class App extends PureComponent {
           Hello, world!
         </h1>
         <input />
-        {this.state.footer}
+        <AsyncFooter />
       </main>
     )
   }
---
+++ src/async-component.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+
+// eslint-disable-next-line no-undef
+const asyncComponent = (getComponent: () => Promise<React$Component>) =>
+  class AsyncComponent extends PureComponent {
+    constructor(props) {
+      super(props)
+      this.state = { Component: null }
+    }
+
+    componentDidMount() {
+      getComponent().then(Component => this.setState({ Component }))
+    }
+
+    render() {
+      const { Component } = this.state
+
+      if (!Component) {
+        return null
+      }
+
+      return <Component {...this.props} />
+    }
+  }
+
+export default asyncComponent
