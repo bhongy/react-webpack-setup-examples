c34b2a98cd3a03018fba6cb6d5fd92ecc9e8299e
Thanik Bhongbhibhat <thanikb@pinterest.com>
2018-03-25 13:08:11 -0700

    HMR works every where (including dynamically loaded chunk - Footer)
    Signed-off-by: Thanik Bhongbhibhat <thanikb@pinterest.com>

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './header.css';
 
-const Header = () => <header className={s.header}>Webpack Demo yo!</header>;
+const Header = () => <header className={s.header}>Webpack Demo yay ~ !</header>;
 
 export default Header;
